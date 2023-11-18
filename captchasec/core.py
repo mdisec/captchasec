711a3908f751e79d2935566c55ec15526de349df
Arif Hamza <80218239+arifhamza@users.noreply.github.com>
2023-11-18 17:59:52 +0400

    Update core.py
    Translate in Azerbaijani language

--- captchasec/core.py
+++ captchasec/core.py
@@ -18,21 +18,21 @@ class CaptchaSec(object):
         self.output = output
         # Checking our credentials
         if not self.solver.check_credentials():
-            print "Your credentials are not VALID..!"
+            print "Məlumatlarınız DOĞRU deyil..!"
             exit(0)
 
         # Print current account balance
-        print "Your current account balance = {}".format(self.solver.get_balance())
+        print "Hal-hazırdakı balansınız = {}".format(self.solver.get_balance())
 
         # Get images from target directory
-        print "Determining of jpeg files from given directory..."
+        print "Verilmiş qovluqdan jpeg fayllarının müəyyən edilir..."
         self.images = self.get_image_list(directory)
 
-        print "Total number of jpeg = {}".format(len(self.images))
+        print "Ümumi jpeg sayı = {}".format(len(self.images))
 
     def get_image_list(self, d):
         """
-        Determines of jpeg files from given directory.
+        Verilmiş qovluqdan jpeg fayllarının müəyyən edilir
         :param d:
         :return:
         """
@@ -45,7 +45,7 @@ class CaptchaSec(object):
 
     def run(self):
         """
-        Send images to the cracker one by one and calculate consumed time while API cracking captcha.
+        API captchanı həll edərkən, siz şəkillərinizi tək-tək həlledici-ə göndərin
         :return:
         """
         for i in self.images:
@@ -54,8 +54,8 @@ class CaptchaSec(object):
             answer = self.solver.solve_image(i)
             end_time = datetime.now()
             total_time = (end_time - start_time).total_seconds()
-            print "Yay! Cracked in {0} second..! Answer is = {1}".format(total_time, answer)
-            self.cracked_captcha.append({'image': i, 'answer': answer, 'time': total_time})
+            print "Urra! {0} saniyədə həll olundu..! Cavab = {1}".format(total_time, answer)
+            self.cracked_captcha.append({'şəkil': i, 'cavab': answer, 'vaxt': total_time})
 
         # Time to generate report.
         report = Report(self.cracked_captcha, self.output)
